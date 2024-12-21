55bb38f59f6b9da1a25a0c4541527e9690cc88a2
walid lhaila <walidlhaila00@gmail.com>
2024-12-21 21:32:31 +0100

    Feat: ADD FUNCTION TO CREATE EMPLOYEES BOTH IN DATABASE AND KEYCLOAK

---
+++ src/employes/dto/keyckloak.dto.ts
@@ -0,0 +1,23 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class KeycloakDto {
+  @IsString()
+  @IsNotEmpty()
+  firstName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  lastName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  username: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+}
--- src/employes/employe.service.ts
+++ src/employes/employe.service.ts
@@ -1,10 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Employes } from './entity/employes.entity';
 import { Repository } from 'typeorm';
 import * as csv from 'csv-parser';
 import { Readable } from 'stream';
 import * as uuid from 'uuid';
+import * as process from 'node:process';
+import axios from 'axios';
+import { KeycloakDto } from './dto/keyckloak.dto';
 
 @Injectable()
 export class EmployeService {
@@ -50,6 +53,17 @@ export class EmployeService {
                   role: 'Employee',
                 });
 
+                const savedEmployee =
+                  await this.employesRepository.save(employee);
+
+                await this.createEmployeesInKeycloak({
+                  username: savedEmployee.username,
+                  email: savedEmployee.email,
+                  firstName: savedEmployee.firstName,
+                  lastName: savedEmployee.lastName,
+                  password: password,
+                });
+
                 employees.push(employee);
               })(),
             );
@@ -57,12 +71,9 @@ export class EmployeService {
           .on('end', async () => {
             await Promise.all(asyncTasks);
 
-            const savedEmployees =
-              await this.employesRepository.save(employees);
-
             resolve({
               message: 'CSV file Imported Successfully',
-              addedCount: savedEmployees.length,
+              addedCount: employees.length,
               skippedCount: skippedEmployees.length,
             });
           })
@@ -73,5 +84,120 @@ export class EmployeService {
         reject(error);
       }
     });
+  }
+
+  private async createEmployeesInKeycloak(
+    employesDto: KeycloakDto,
+  ): Promise<void> {
+    const keycloakUrl = process.env.KEYCLOAK_URL;
+    const realm = process.env.KEYCLOAK_REALM;
+    const clientId = process.env.KEYCLOAK_CLIENT_ID;
+    const clientSecret = process.env.KEYCLOAK_CLIENT_SECRET;
+
+    try {
+      console.log('Attempting to get access token...');
+      const tokenResponse = await axios.post(
+        `${keycloakUrl}/realms/${realm}/protocol/openid-connect/token`,
+        new URLSearchParams({
+          grant_type: 'client_credentials',
+          client_id: clientId,
+          client_secret: clientSecret,
+        }),
+        {
+          headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        },
+      );
+
+      const accessToken = tokenResponse.data.access_token;
+      console.log('Access token obtained successfully');
+      console.log('Attempting to create user in Keycloak...');
+
+      const createEmployeesResponse = await axios.post(
+        `${keycloakUrl}/admin/realms/${realm}/users`,
+        {
+          username: employesDto.username,
+          email: employesDto.email,
+          firstName: employesDto.firstName,
+          lastName: employesDto.lastName,
+          enabled: true,
+          credentials: [
+            {
+              type: 'password',
+              value: employesDto.password,
+              temporary: false,
+            },
+          ],
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+            'Content-Type': 'application/json',
+          },
+        },
+      );
+
+      console.log('Employees created successfully in Keycloak');
+
+      const employeId = createEmployeesResponse.headers.location
+        .split('/')
+        .pop();
+
+      const employeesRoleId = await this.getEmployeeRoleId(
+        keycloakUrl,
+        realm,
+        accessToken,
+      );
+
+      await axios.post(
+        `${keycloakUrl}/admin/realms/${realm}/users/${employeId}/role-mappings/realm`,
+        [{ id: employeesRoleId, name: 'Employee' }],
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+            'Content-Type': 'application/json',
+          },
+        },
+      );
+
+      console.log('Employee role assigned to user in Keycloak');
+    } catch (error) {
+      console.error('Detailed error in Keycloak user creation:');
+      if (error.response) {
+        console.error('Response data:', error.response.data);
+        console.error('Response status:', error.response.status);
+        console.error('Response headers:', error.response.headers);
+      } else if (error.request) {
+        console.error('No response received:', error.request);
+      } else {
+        console.error('Error message:', error.message);
+      }
+      throw new HttpException(
+        `Failed to create user in Keycloak: ${error.message}`,
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  private async getEmployeeRoleId(
+    keycloakUrl: string,
+    realm: string,
+    accessToken: string,
+  ): Promise<string> {
+    const rolesResponse = await axios.get(
+      `${keycloakUrl}/admin/realms/${realm}/roles`,
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      },
+    );
+
+    const employeeRole = rolesResponse.data.find(
+      (role) => role.name === 'Employee',
+    );
+    if (!employeeRole) {
+      throw new Error('HR Role Not Found In Keycloak');
+    }
+    return employeeRole.id;
   }
 }
